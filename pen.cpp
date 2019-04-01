a0df3972403ec921796c827ca5cef40fb0820042
Fast Engine <49161771+fastengine@users.noreply.github.com>
2019-04-01 13:42:35 -0300

    Source files part 2

---
+++ pen.cpp
@@ -0,0 +1,22 @@
+// Fast Engine Graphical User Interface (F.E.G.U.I)
+// This feature is a part of FastEngine
+// by Mario Freire
+// Copyright (C) 2012 DSP Interactive. All rights reserved.
+// Development Systems and Programs
+// See more information in the website: http://www.dspinteractive.com
+
+#include "pen.h" // class's header file
+
+// class constructor
+TPen::TPen()
+{
+width = 1;
+style = psSolid;
+color = clBlack;
+}
+
+// class destructor
+TPen::~TPen()
+{
+	// insert your code here
+}
